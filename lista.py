d6281a606933d82515d8c8ab0170c5e9e1f3fd15
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-03-05 09:35:26 -0300

    Create lista.py

---
+++ lista.py
@@ -0,0 +1,61 @@
+# Estruturas de dados lineares
+# Listas []
+'''
+vazio = []
+vazio_2 = list()
+
+numeros = [5,3,8,9,6]
+nomes = ["Kleber", "Manuela", 'Nicolas']
+mista = ["Kleber", 26, "01234567890", 1.85, 142, True]
+
+letras = list("Paralelepípedo")
+print(letras[2])
+print(letras[-5])
+print(letras[4:8]) 
+# Intervalo, numero da esquerda inclusivo
+# numero da direita exclusivo
+invertida = letras[::-1]
+print(invertida)
+'''
+
+'''
+# Adicinar elementos
+carros =["Fusca", "Santana", "Opala"]
+print(carros)
+carros.append("Maverick")   # Adiciona no final
+print(carros)
+carros.insert(2,"Passat")   # Adicina na posição do índice
+print(carros)
+carros.extend(["Escort", "Chevette"]) # Adiciona uma nova lista no fim
+print(carros)
+'''
+
+# Remover elementos
+tarefas = [
+    "Comprar",
+    "Reabastecer",
+    "Limpar",
+    "Ler",
+    "Comprar",
+    "Responder"
+    ]
+print(tarefas)
+
+tarefas.remove("Reabastecer")   # Remove a primeira ocorrencia
+print(tarefas)
+
+ultima_tarefa = tarefas.pop()   # Remove o último e pega o retorno
+print(ultima_tarefa)
+print(tarefas)
+
+del tarefas[1]                  # Deleta no índice indicado
+print(tarefas)
+
+t = "Comprar"
+while t in tarefas:             # Remove todas as ocorrências
+    tarefas.remove(t)
+
+print(tarefas)
+
+tarefas.clear()
+print(tarefas)
